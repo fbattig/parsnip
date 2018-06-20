efdb783cb86659d80f984580e5dcd22ba322b0f5
Felix Battig <fbattig@yahoo.com>
2018-06-20 10:25:00 -0400

    Installed react-redux implemented Provicer and connect

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import React, { Component } from 'react';
+import {connect } from 'react-redux';
+
 import TaskPage from './components/TasksPage';
 
-const mockTasks = [
-  {
-    id: 1,
-    title: 'Learn Redux',
-    description: 'The store, actions, and reducers, oh my!',
-    status: 'Unstarted',
-  },
-  {
-    id: 2,
-    title: 'Peace on Earth',
-    description: 'No big deal.',
-    status: 'In Progress',
-  },
-];
 
 
 class App extends Component {
   render() {
     return (
       <div className='main-content'>
-        <TaskPage tasks ={mockTasks} />
+        <TaskPage tasks ={this.props.tasks} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    tasks: state.tasks
+  };
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import { createStore } from 'redux';
+import tasks from './reducers';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(tasks);
+
+ReactDOM.render(
+<Provider store={store}>
+    <App  />
+</Provider>,
+ document.getElementById('root'));
 registerServiceWorker();
